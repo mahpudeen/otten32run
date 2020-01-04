12b45f325e3c9b64147da00cfd79b023a6f27a9d
Dindin Mahpudeen <mahpudeen@gmail.com>
2020-01-04 18:44:19 +0700

    Test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <div class="navbar-header">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+      <div class="container">
+        <div id="navbarNav" class="navbar-collapse collapse" style>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a href="#" class="nav-link">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">TENTANG OTTEN32RUN</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">INFORMASI UMUM</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">PENDAFTARAN</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">PERATURAN UMUM</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">CONTACT US</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div>
+    <b-img src="../assets/img/banner.gif" fluid-grow alt="Fluid-grow image"></b-img>
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
