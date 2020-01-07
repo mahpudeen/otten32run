9811ec9beb040c2bc5dfca9078c7ea71ffcb3485
Dindin Mahpudeen <mahpudeen@gmail.com>
2020-01-07 17:10:03 +0700

    Landing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,42 +12,5 @@ export default {
             
         }
     },
-
-    created() {
-        // let Popper = document.createElement('script');    
-        // Popper.setAttribute('src',"statics/js/plugins/popper/popper.min.js");
-        // document.body.appendChild(Popper);
-        
-        // let bootstrapJs = document.createElement('script');    
-        // bootstrapJs.setAttribute('src',"statics/js/bootstrap.js");
-        // document.body.appendChild(bootstrapJs);
-
-        // let metisMenu = document.createElement('script');    
-        // metisMenu.setAttribute('src',"statics/js/plugins/metisMenu/jquery.metisMenu.js");
-        // document.body.appendChild(metisMenu);
-
-        // let slimScroll = document.createElement('script');    
-        // slimScroll.setAttribute('src',"statics/js/plugins/slimscroll/jquery.slimscroll.min.js");
-        // document.body.appendChild(slimScroll);
-
-        // let Inspinia = document.createElement('script');    
-        // Inspinia.setAttribute('src',"statics/js/inspinia.js");
-        // document.body.appendChild(Inspinia);
-
-        // let Wow = document.createElement('script');    
-        // Wow.setAttribute('src',"statics/js/plugins/wow/wow.min.js");
-        // document.body.appendChild(Wow);
-        
-        // let Pace = document.createElement('script');    
-        // Pace.setAttribute('src',"statics/js/plugins/pace/pace.min.js");
-        // document.body.appendChild(Pace);
-
-        // let landing = document.createElement('script');    
-        // landing.setAttribute('src',"statics/js/landing.js");
-        // document.body.appendChild(landing);
-
-        //EndJs
-    },
-
 }
 </script>
