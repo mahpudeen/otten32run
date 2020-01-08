ff127592b01a3e7215d9bd03ab2e23e9c218e5e8
Dindin Mahpudeen <mahpudeen@gmail.com>
2020-01-08 23:03:27 +0700

    template

---
+++ src/views/templateFileBaru.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- DIV JUDUL -->
+    <div class="ibox-title">
+      <h5>Title</h5>
+    </div>
+    <div class="ibox-content">
+    <!-- DIV BODY, MASUKIN CODINGAN DISINI (DALAM IBOX-CONTENT) -->
+      
+    <!-- BATAS CODINGAN -->
+    </div>
+  </div>
+</template>
