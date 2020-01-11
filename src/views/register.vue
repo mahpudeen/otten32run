62ddef7db55604931448a5ab17d312652472fa6d
Gina Rizka Alsyah <ginarizkaa@gmail.com>
2020-01-11 15:22:25 +0700

    api regis

--- src/views/register.vue
+++ src/views/register.vue
@@ -39,66 +39,41 @@
           <div class="col-lg-3 wow zoomIn"></div>
 
           <div class="col-lg-6 wow zoomIn">
-            <form>
-              <div class="form-group">
-                <label for="nama_user">Nama Lengkap</label>
-                <input type="text" class="form-control" id="nama_user" autofocus />
-              </div>
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="jenis_kelamin">Jenis Kelamin</label>
-                    <input type="text" class="form-control" id="jenis_kelamin" />
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="gol_darah">Golongan Darah</label>
-                    <input type="text" class="form-control" id="gol_darah" />
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  autofocus
-                />
-                <small
-                  id="emailHelp"
-                  class="form-text text-muted"
-                >We'll never share your email with anyone else.</small>
-              </div>
-              <div class="form-group">
-                <label for="alamat_user">Alamat</label>
-                <input type="text" class="form-control" id="alamat_user" />
-              </div>
-              <div class="form-group">
-                <label for="riwayat_kes_user">Riwayat Kesehatan</label>
-                <input type="text" class="form-control" id="riwayat_kes_user" />
-              </div>
-              <div class="form-group">
-                <label for="riwayat_keskel_user">Riwayat Kesehatan Keluarga</label>
-                <input type="text" class="form-control" id="riwayat_keskel_user" />
-              </div>
-              <div class="form-group">
-                <label for="obat_pribadi_user">Obat - obatan Pribadi</label>
-                <input type="text" class="form-control" id="obat_pribadi_user" />
-              </div>
-              <div class="form-group">
-                <label for="size_baju_user">Size Kaos</label>
-                <input type="text" class="form-control" id="size_baju_user" />
-              </div>
-              
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" />
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <div class="form-group">
+              <label for="nama_user">Nama Lengkap</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="param.nama_lengkap"
+                autofocus
+                required
+              />
+            </div>
+            <div class="form-group">
+              <label for="nama_user">Nomor Handphone</label>
+              <input type="text" class="form-control" v-model="param.nomor_hp" required />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                aria-describedby="emailHelp"
+                v-model="param.email_user"
+                required
+              />
+              <small
+                id="emailHelp"
+                class="form-text text-muted"
+              >We'll never share your email with anyone else.</small>
+            </div>
+
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" v-model="param.password_user" required />
+            </div>
+            <button @click="regisProcess()" class="btn btn-primary">Submit</button> &nbsp;
+            <button @click="resetForm()" class="btn btn-warning">Reset</button>
           </div>
         </div>
       </div>
@@ -115,19 +90,8 @@
           </div>
         </div>
         <div class="row m-b-lg justify-content-center">
-          <div class="col-lg-3">
-            <!-- <address>
-                        <strong><span class="navy">Company name, Inc.</span></strong><br/>
-                        795 Folsom Ave, Suite 600<br/>
-                        San Francisco, CA 94107<br/>
-                        <abbr title="Phone">P:</abbr> (123) 456-7890
-            </address>-->
-          </div>
-          <div class="col-lg-4">
-            <!-- <p class="text-color">
-                        Consectetur adipisicing elit. Aut eaque, totam corporis laboriosam veritatis quis ad perspiciatis, totam corporis laboriosam veritatis, consectetur adipisicing elit quos non quis ad perspiciatis, totam corporis ea,
-            </p>-->
-          </div>
+          <div class="col-lg-3"></div>
+          <div class="col-lg-4"></div>
         </div>
         <div class="row">
           <div class="col-lg-12 text-center">
@@ -177,3 +141,60 @@
   margin: 20px;
 }
 </style>
+<script>
+import Swal from "sweetalert2";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      param: {
+        nama_lengkap: "",
+        email_user: "",
+        nomor_hp: "",
+        password_user: ""
+      }
+    };
+  },
+  methods: {
+    resetForm() {
+      this.param = {
+        nama_lengkap: "",
+        email_user: "",
+        nomor_hp: "",
+        password_user: ""
+      };
+    },
+    regisProcess() {
+      let self = this;
+
+      Swal.fire({
+        title: "Konfirmasi",
+        text: "Apakah Anda yakin telah mengisi dengan benar?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Ya!",
+        cancelButtonText: "Tidak!"
+      }).then(result => {
+        if (result.value) {
+          axios
+            .post("http://localhost/api/public/regis_user/", self.param)
+            .then(function(datas) {
+              return datas;
+            })
+            .then(function(res) {
+              Swal.fire("Berhasil!", "Data berhasil disimpan!", "success");
+              self.resetForm();
+            })
+            .catch(function(err) {
+              console.log(err);
+              Swal.fire("Gagal!", "Data tidak berhasil disimpan!", "error");
+            });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          // Swal.fire("Cancelled", "Your imaginary file is safe :)", "error");
+        }
+      });
+    }
+  }
+};
+</script>
